693a366ee9c5f13193dc0caa8994f02985a772a6
Ankit Kumar <kumar805231@gmail.com>
2018-03-01 19:06:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/login', function(req, res){
             res.status(500).send(errr.toString());
         } else {
             if(result.rows.length === 0) {
-                res.status(403).send('username/password is invalid !');
+                res.status(403).send('username/password is invalid');
             } else {
                 var dbString = result.rows[0].password;
                 var salt = dbString.split('$')[2];
@@ -95,7 +95,7 @@ app.post('/login', function(req, res){
                 if(hashedPassword === dbString){
                     res.send('Credentials Correct !');
                 } else {
-                    res.send(403).send('username|password is invalid !');
+                    res.send(403).send('username|password is invalid');
                 }
             }
         }
