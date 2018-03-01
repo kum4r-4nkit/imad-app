24b045be04ec60a0e53194652d5026845d9cad38
Ankit Kumar <kumar805231@gmail.com>
2018-03-01 19:08:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ submit.onclick = function () {
             if(request.status === 200){
                 alert('Logged in Successfully');
             } else if ((request.status === 403)){
-                alert('username/password is invalid');
+                alert('username#password is invalid');
             } else if ((request.status === 500)){
                 alert('Something went worng');
             }
