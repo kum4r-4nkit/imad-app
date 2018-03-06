7c29864f940c509932c062a29291c683b4cd7b24
Ankit Kumar <kumar805231@gmail.com>
2018-03-06 21:35:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,5 +23,5 @@ submit.onclick = function () {
     console.log('password');
     request.open('POST','http://kumar805231.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username : username, password : password}));
+    request.send(JSON.stringify({username: username, password: password}));
 };
